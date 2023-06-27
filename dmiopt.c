ff8a8f5d4c92b0c20747f68f76ed3f5b7dae27b9
Jiri Hnidek <jhnidek@redhat.com>
2023-06-27 09:46:02 +0200

    Added --json CLI option. It does nothing ATM.

--- dmiopt.c
+++ dmiopt.c
@@ -265,7 +265,7 @@ static u32 parse_opt_handle(const char *arg)
 int parse_command_line(int argc, char * const argv[])
 {
 	int option;
-	const char *optstring = "d:hqs:t:uH:V";
+	const char *optstring = "d:hqs:t:uH:jV";
 	struct option longopts[] = {
 		{ "dev-mem", required_argument, NULL, 'd' },
 		{ "help", no_argument, NULL, 'h' },
@@ -279,6 +279,7 @@ int parse_command_line(int argc, char * const argv[])
 		{ "handle", required_argument, NULL, 'H' },
 		{ "oem-string", required_argument, NULL, 'O' },
 		{ "no-sysfs", no_argument, NULL, 'S' },
+        { "json", no_argument, NULL, 'j' },
 		{ "version", no_argument, NULL, 'V' },
 		{ NULL, 0, NULL, 0 }
 	};
@@ -332,6 +333,9 @@ int parse_command_line(int argc, char * const argv[])
 			case 'S':
 				opt.flags |= FLAG_NO_SYSFS;
 				break;
+            case 'j':
+                opt.flags |= FLAG_JSON;
+                break;
 			case 'V':
 				opt.flags |= FLAG_VERSION;
 				break;
@@ -384,6 +388,7 @@ void print_help(void)
 		"     --from-dump FILE   Read the DMI data from a binary file\n"
 		"     --no-sysfs         Do not attempt to read DMI data from sysfs files\n"
 		"     --oem-string N     Only display the value of the given OEM string\n"
+        " -j, --json             Output information in JSON format\n"
 		" -V, --version          Display the version and exit\n";
 
 	printf("%s", help);
